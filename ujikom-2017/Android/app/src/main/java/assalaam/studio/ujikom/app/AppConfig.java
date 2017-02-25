c5321e0424ae99a8cec1520895be323146007884
smk-141510249 <141510249@smkassalaambandung.sch.id>
2017-02-25 07:29:56 +0700

    laravel dan android

---
+++ ujikom-2017/Android/app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.2.38/ujikom4-141510249-master/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
